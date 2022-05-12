f00e9f42c7c3d213b5e505bc388d996475193da2
massaillon <96022653+massaillon@users.noreply.github.com>
2022-05-12 10:56:14 +0000

    Update CSS ,HTML and JS files,Add new features

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-
-
-function mobileMenu(){
-  /*Open the menu and and change the Hamburger to a X on click */
-if(navMenu.classList.contains('hide')){
+function mobileMenu() {
+  /* Open the menu and and change the Hamburger to a X on click */
+  if (navMenu.classList.contains('hide')) {
     navMenu.classList.remove('hide');
-    hamburger.classList.toggle("active")
-}
-else {
+    hamburger.classList.toggle('active');
+  } else {
     navMenu.classList.add('hide');
-    hamburger.classList.toggle("active");
+    hamburger.classList.toggle('active');
+  }
 }
-
-}
-
-hamburger.addEventListener("click", mobileMenu);
-/*For closing navigation on the click of each link*/
+hamburger.addEventListener('click', mobileMenu);
+/* For closing navigation on the click of each link */
+document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
+  hamburger.classList.remove('active');
+  navMenu.classList.add('hide');
+}));
