c531a3a458344a44e21eb8645f6e8ae54a0b09eb
massaillon <96022653+massaillon@users.noreply.github.com>
2022-05-19 09:39:31 +0000

    Update CSS,HTML,JS files as per project requirements

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const hamburger = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-menu');
-const blurAll = document.querySelector('blur');
+const modalWindow = document.querySelector('.modal');
+const modalButton1 = document.querySelector('.seeProjBtn1');
+const modalButton2 = document.querySelector('.seeProjBtn2');
+const closeModal = document.querySelector('.close');
+
 function mobileMenu() {
   /* Open the menu and and change the Hamburger to a X on click */
   if (navMenu.classList.contains('hide')) {
@@ -9,7 +13,6 @@ function mobileMenu() {
   } else {
     navMenu.classList.add('hide');
     hamburger.classList.toggle('active');
-    blurAll.classList.remove('blur');
   }
 }
 hamburger.addEventListener('click', mobileMenu);
@@ -18,3 +21,21 @@ document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click'
   hamburger.classList.remove('active');
   navMenu.classList.add('hide');
 }));
+
+function modalPopup() {
+  /* Open the modal popup window on click */
+  if (modalWindow.classList.contains('hide')) {
+    modalWindow.classList.remove('hide');
+  } else {
+    modalWindow.classList.add('hide');
+  }
+}
+/* modalButton.forEach(n => n.addEventListener('click', modalPopup)) */
+modalButton1.addEventListener('click', modalPopup);
+modalButton2.addEventListener('click', modalPopup);
+closeModal.addEventListener('click', modalPopup);
+modalWindow.addEventListener('click', (c) => {
+  if (c.target.matches('.modal')) {
+    modalPopup();
+  }
+});
